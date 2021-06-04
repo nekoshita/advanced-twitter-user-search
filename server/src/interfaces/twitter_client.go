407717345ae9b878391b5948b9a78392b113e101
Nekoshita Yuki <yuki.303442@gmail.com>
2021-06-05 01:57:19 +0900

    feat: define twitter client interface

---
+++ server/src/interfaces/twitter_client.go
@@ -0,0 +1,11 @@
+package interfaces
+
+import (
+	"context"
+
+	"github.com/nekoshita/advanced-twitter-user-search/server/src/domain"
+)
+
+type TwitterClient interface {
+	Search(ctx context.Context, query string, page int) ([]domain.User, error)
+}
